fd90a3c6055b9428b767c35c84f9be396a39bdbb
PRONOY <pronoybanik82@gmail.com>
2023-06-09 22:16:17 +0600

    some change header

--- src/conponent/Pages/Bannar/BannerItem/BannerItem.js
+++ src/conponent/Pages/Bannar/BannerItem/BannerItem.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 const BannerItem = ({slide}) => {
     const {image, prev, id, next} = slide;
+    
     return (
         <div id= {`slide${id}`} className="carousel-item relative w-full ">
                     <div className='carousel-img'>
 
-                        <img src={image} className="w-full rounded-xl h-full" />
+                        <img src={image} className="w-full rounded-xl h-full" alt=''/>
                     </div>
 
                     <div className="absolute  flex justify-end transform -translate-y-1/2 left-5  top-1/3">
-                        <h1 className='font-bold  text-white text-5xl'>
+                        <h1 className='font-bold  text-white lg:text-5xl text-xl'>
                             NATURAL <p className='text-orange-600'>BLEND</p>
                         </h1>
                     </div>
                     <div className="absolute flex justify-end transform  w-1/2 -translate-y-1/2 left-5  top-2/4">
-                        <p className='text-white mt-8' > If you find that your favorite music or your best-loved movie director no longer stirs your imagination, it’s time to turn your attention to the all-time classics: the famous photographers that shook the world. </p>
+                        <p className='text-white mt-24 lg:text-xl text-xs w-full ' > If you find that your favorite music or your best-loved movie director no longer stirs your imagination, it’s time to turn your attention to the all-time classics: the famous photographers that shook the world. </p>
                     </div>
                     
                     <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
